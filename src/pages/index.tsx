1a0723ca400862048359f2352df876f660c29ad1
Jesus Ordosgoitty <jesuodz@gmail.com>
2023-05-14 01:39:00 -0400

    fix crossOrigin attribute

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,8 +10,7 @@ export default function Home() {
 				<meta name="description" content="Jesus Ordosgoitty - Software Developer" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="preconnect" href="https://fonts.googleapis.com"  />
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-				<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />				<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<Layout>
