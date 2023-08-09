1e5bf47fd5a61c37173ed6a2a0bb9e225bbe5d6e
Jesus Ordosgoitty <jesuodz@gmail.com>
2023-08-09 16:38:42 -0400

    finish 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,31 @@
+import NextLink from 'next/link'
+import Layout from "@/layouts/main"
+import { Link, Stack, Typography } from "@mui/material"
+import { useTranslation } from 'react-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { GetStaticProps } from 'next'
+
+const NotFoundPage = () => {
+    const { t } = useTranslation()
+
+    return (
+        <Layout>
+            <Stack spacing={2} alignSelf='center'>
+                <Typography variant="subtitle1" fontWeight={900}>
+                    {t('not-found')}
+                </Typography>
+                <Link href="/" component={NextLink}>{t('back')}</Link>
+            </Stack>
+        </Layout>
+    )
+}
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale ?? 'en', ['common', 'notfound'])),
+    },
+  }
+}
+
+export default NotFoundPage
