c3495fffd096f8a8e3b7c144b29880e8f174b261
Jesus Ordosgoitty <jesuodz@gmail.com>
2024-03-07 13:41:50 -0400

    add google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import i18nextConfig from '../../next-i18next.config.js'
+import Script from 'next/script'
 
 export default function Document(props: any) {
     const currentLocale =
@@ -8,7 +9,23 @@ export default function Document(props: any) {
 
     return (
         <Html lang={currentLocale}>
-          <Head />
+          <Head>
+                <Script
+                    async
+                    src='https://www.googletagmanager.com/gtag/js?id=G-B0Q9QLJ4ES'
+                />
+                <Script id="google-analytics">
+                    {
+                        `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+
+                        gtag('config', 'G-B0Q9QLJ4ES');
+                        `
+                    }
+                </Script>
+           </Head>
           <body>
             <Main />
             <NextScript />
