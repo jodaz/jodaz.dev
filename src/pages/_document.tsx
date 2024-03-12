51dc7823d2dbdf0448347ce298c06963ccc0e941
Jesus Ordosgoitty <jesuodz@gmail.com>
2024-03-12 10:25:21 -0400

    update analytics config

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import i18nextConfig from '../../next-i18next.config.js'
+import Script from "next/script";
 
 export default function Document(props: any) {
     const currentLocale =
@@ -9,20 +10,23 @@ export default function Document(props: any) {
     return (
         <Html lang={currentLocale}>
           <Head>
-                <script
-                    async
-                    src="https://www.googletagmanager.com/gtag/js?id=G-B0Q9QLJ4ES"
-                />
-                <script>
-                    {`
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-
-                        gtag('config', 'G-B0Q9QLJ4ES');
-                    `}
-                </script>
-           </Head>
+            <Script
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=G-B0Q9QLJ4ES`}
+            />
+            <Script strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                      gtag('config', 'G-B0Q9QLJ4ES', {
+                        page_path: window.location.pathname,
+                      });
+                    `,
+                }}
+            />
+          </Head>
           <body>
             <Main />
             <NextScript />
